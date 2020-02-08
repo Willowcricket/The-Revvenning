7c3b36c3384ee4a127d9499787a6b4ecfd671d1f
Willow <willowcrickit@gmail.com>
2020-02-07 19:37:12 -0700

    Player and Looping Background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Background : MonoBehaviour
+{
+    public Transform tf;
+    public Vector3 tfpos = new Vector3(10.85f,0.0f,0.0f);
+    public float movementSpeed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tf.position -= tf.right * movementSpeed * Time.deltaTime;
+
+        if (this.transform.position.x <= -10.85f)
+        {
+            Debug.Log("Background Has Run Out of Room");
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        tf.position = tfpos;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform tf;
+    public float movementSpeed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.up * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position -= tf.up * movementSpeed * Time.deltaTime;
+        }
+    }
+
+}
